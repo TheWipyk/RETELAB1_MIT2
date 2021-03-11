94b32b3d57035d6ecfc1caa15655a4e72100b66c
Botond Pércsi <thewipyk@gmail.com>
2021-03-11 15:49:30 +0100

    task 4.4 done

--- hu.bme.mit.yakindu.analysis/src/hu/bme/mit/yakindu/analysis/workhere/Main.java
+++ hu.bme.mit.yakindu.analysis/src/hu/bme/mit/yakindu/analysis/workhere/Main.java
@@ -6,8 +6,11 @@ import org.junit.Test;
 import org.yakindu.sct.model.sgraph.State;
 import org.yakindu.sct.model.sgraph.Statechart;
 import org.yakindu.sct.model.sgraph.Transition;
+import org.yakindu.sct.model.stext.stext.EventDefinition;
+import org.yakindu.sct.model.stext.stext.VariableDefinition;
 
 import hu.bme.mit.model2gml.Model2GML;
+import hu.bme.mit.yakindu.analysis.example.IExampleStatemachine;
 import hu.bme.mit.yakindu.analysis.modelmanager.ModelManager;
 
 public class Main {
@@ -26,32 +29,29 @@ public class Main {
 		// Reading model
 		Statechart s = (Statechart) root;
 		TreeIterator<EObject> iterator = s.eAllContents();
+		System.out.println("public static void print (IExampleStatemachine s) {");
 		while (iterator.hasNext()) {
-			EObject content = iterator.next();
-			if(content instanceof State) {
-				State state = (State) content;
-				System.out.println(state.getName());
-				if(state.getOutgoingTransitions().isEmpty()) {
-					System.out.println("The state \""+state.getName()+"\" is a trap!");
+			EObject content = iterator.next();			
+			if(content instanceof VariableDefinition) {
+				VariableDefinition varDef = (VariableDefinition)content;
+				String cap = varDef.getName().substring(0,1).toUpperCase() + varDef.getName().substring(1);
+				char shortName = cap.charAt(0);
+				System.out.println("System.out.println(\""+shortName+ "= \" + s.getSCInterface().get" + cap+"());");
 				}
-			}
-			if(content instanceof Transition) {
-				Transition transition = (Transition) content;
-				String sourceState = transition.getSource().getName();
-				String destState = transition.getTarget().getName();
-				if(destState.isEmpty()) {		
-					
-					System.out.println("No state name found.  Using recommended name:\"" + transition.getSpecification().toString()+"\"");
-					destState = transition.getSpecification().toString();
-					transition.getTarget().setName(destState);
+			if(content instanceof EventDefinition) {
+				EventDefinition evDef = (EventDefinition)content;
+				String cap = evDef.getName().substring(0,1).toUpperCase() + evDef.getName().substring(1);
+				char shortName = cap.charAt(0);
+				System.out.println("System.out.println(\"Event_"+cap+ "= \" + s.getSCInterface().get" + cap+"());");
 				}
-				System.out.println(sourceState + "->" + destState);
-			}
-		}
-		
+			
+		}		
+		System.out.println("}");
 		// Transforming the model into a graph representation
 		String content = model2gml.transform(root);
 		// and saving it
 		manager.saveFile("model_output/graph.gml", content);
 	}
+	
+	
 }
